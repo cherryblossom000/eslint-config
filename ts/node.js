bd6d839f53b53712b0137c38a711c65ddf4866d8
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2020-05-30 16:59:51 +1000

    fix: fix crash

--- ts/node.js
+++ ts/node.js
@@ -1,20 +1,11 @@
 'use strict'
 
-const {rules: importRules} = require('./_import')
-
-const {'node/file-extension-in-import': fileExtensionInImport} = importRules
-
 module.exports = {
   extends: [
     '../js/node',
     '.'
   ],
   rules: {
-    'node/file-extension-in-import': [
-      ...fileExtensionInImport.slice(0, 2),
-      {...fileExtensionInImport[2], tryExtensions: ['.js', '.json', '.ts']}
-    ],
-
     // Already checked by TS
     'node/process-exit-as-throw': 0,
 

4a914afef7ea20ff427e099427d7c742ec990185
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-05-08 12:40:30 +1000

    feat(ts/jest): add `jest/unbound-method`
    Add the `jest/unbound-method` rule for the `ts/jest` config.
    
    BREAKING CHANGE: Requires `eslint-plugin-jest@^24.3`.

--- ts/jest.js
+++ ts/jest.js
@@ -1,5 +1,11 @@
 'use strict'
 
+const {rules} = require('.')
+
 module.exports = {
-  extends: '../js/jest'
+  extends: '../js/jest',
+  rules: {
+    '@typescript-eslint/unbound-method': 0,
+    'jest/unbound-method': rules['@typescript-eslint/unbound-method']
+  }
 }

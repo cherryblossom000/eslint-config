fc168823dd3d02f312b076697c86f9879c73e96f
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-12-26 13:24:04 +1100

    feat: enable es2022 rules for node/16 config

--- node/16.js
+++ node/16.js
@@ -1,9 +1,9 @@
 'use strict'
 
 module.exports = {
-  extends: '../_mixins/es/2021',
+  extends: ['../_mixins/es/2021', '../_mixins/es/2022'],
   env: {es2021: true},
-  parserOptions: {ecmaVersion: 2021},
+  parserOptions: {ecmaVersion: 2022},
   rules: {
     'unicorn/prefer-node-protocol': 1
   }

5d9e68b55c5bde2aab9b454a4d38c7684b95419c
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2022-04-10 13:42:28 +1000

    feat: enable `unicorn/prefer-modern-math-apis`

--- js/index.js
+++ js/index.js
@@ -289,6 +289,7 @@ module.exports = {
     'unicorn/prefer-export-from': 1,
     'unicorn/prefer-includes': 2,
     'unicorn/prefer-math-trunc': 2,
+    'unicorn/prefer-modern-math-apis': 2,
     'unicorn/prefer-negative-index': 2,
     'unicorn/prefer-object-from-entries': 2,
     'unicorn/prefer-optional-catch-binding': 1,
